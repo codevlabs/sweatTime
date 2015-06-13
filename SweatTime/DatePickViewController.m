2dd0ce87be315f29b8305944e76faef11553623b
Lye Guang Xing <gxlye@me.com>
2015-06-14 05:10:25 +0800

    Added AppIcon, Splashscreen and Startup Page

--- SweatTime/DatePickViewController.m
+++ SweatTime/DatePickViewController.m
@@ -139,6 +139,8 @@ NSUserDefaults *defaults;
     CGRect toolbarTargetFrame = CGRectMake(0, self.view.bounds.size.height, 320, 44);
     CGRect datePickerTargetFrame = CGRectMake(0, self.view.bounds.size.height+44, 320, 216);
     [UIView beginAnimations:@"MoveOut" context:nil];
+    [self.view viewWithTag:52].alpha = 0;
+    [self.view viewWithTag:53].alpha = 0;
     [self.view viewWithTag:9].alpha = 0;
     [self.view viewWithTag:10].frame = datePickerTargetFrame;
     [self.view viewWithTag:11].frame = toolbarTargetFrame;
